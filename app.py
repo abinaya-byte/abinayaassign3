80b7e1ed78a4be8867c74c66530b2c7ff189cb8b
abinaya-byte <76507395+abinaya-byte@users.noreply.github.com>
2024-04-11 13:42:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-# Use the official Python image from Docker Hub as the base image
-FROM python:3.8
-
-# Set the working directory in the container
-WORKDIR /app
-
-# Copy the requirements file into the container at /app
-COPY requirements.txt requirements.txt
-
-# Install dependencies
-RUN pip install -r requirements.txt
-
-# Copy the current directory contents into the container at /app
-COPY . .
-
-# Define environment variable
-ENV FLASK_APP=app.py
-
-# Expose port 5000 to allow communication to/from the Flask app
-EXPOSE 80
-
-# Command to run the Flask application
-CMD ["flask", "run", "--host=0.0.0.0"]
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_cloud():
+    return 'Hello from Abinaya ECS container!'
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
